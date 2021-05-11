e05085c0ced62c377739f60b9a9a71f9d6fb0f41
vikas_kumar_verma <vikasverma5392@gmail.com>
2021-05-11 15:37:16 +0530

    Update AgeCalculator.java

--- assignment11-age calculator/AgeCalculator.java
+++ assignment11-age calculator/AgeCalculator.java
@@ -1,37 +1,30 @@
 
 import java.time.LocalDate;
-
+import java.time.Period;
 import java.util.Scanner;
 
-class Year{ int yearofbirth;
-            int monthofbirth;
-            int dayofbirth;
+class Age{
+    String dob;
+    void ageFinder() {
 
-   void age(int y,int m,int d){
-       LocalDate datetoday =LocalDate.now();
-       int yeartoday=datetoday.getYear();
-       int ageyear=yeartoday-y;
-       int monthtoday=datetoday.getMonthValue();
-       int agemonth=monthtoday-m;
-       int daytoday=datetoday.getDayOfMonth();
-       int ageday=daytoday-d;
+                LocalDate localdateofbirth=LocalDate.parse(dob);
+                LocalDate todaysdate=LocalDate.now();
 
-       System.out.println("The age is "+ageyear+" years "+agemonth+" months "+ageday+" days.");
-   }
+        int years=Period.between(localdateofbirth,todaysdate).getYears();
+        int months=Period.between(localdateofbirth,todaysdate).getMonths();
+        int days=Period.between(localdateofbirth,todaysdate).getDays();
+
+        System.out.println("Your age is "+years+ " years "+months+ " months "+ days+" days.");
+    }
 }
 
 public class AgeCalculator {
 
     public static void main(String[] args) {
-        Year objectyear=new Year();
+        Age object=new Age();
         Scanner input=new Scanner(System.in);
-        System.out.println("Enter date of birth (dd/mm/yyyy):");
-        String dob=input.next();
-        objectyear.dayofbirth=Integer.parseInt(dob.substring(0,2));
-        objectyear.monthofbirth=Integer.parseInt(dob.substring(3,5));
-        objectyear.yearofbirth=Integer.parseInt(dob.substring(6));
-
-        objectyear.age(objectyear.yearofbirth,objectyear.monthofbirth,objectyear.dayofbirth);
-
+        System.out.println("Enter your date of birth(yyyy-mm-dd)");
+        object.dob=input.next();
+        object.ageFinder();
     }
 }
