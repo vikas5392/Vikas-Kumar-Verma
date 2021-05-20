c13a60239a0a6c1bd848ea6c295df9498074492b
vikas_kumar_verma <vikasverma5392@gmail.com>
2021-05-20 20:54:16 +0530

    Update DisplayFamily.java

--- assignment19-Family/DisplayFamily.java
+++ assignment19-Family/DisplayFamily.java
@@ -1,128 +1,81 @@
 
-abstract class Family{             
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    private String name;           
-    private int age;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
+class FamilyMember{
+   String name;
+    int age;
+    String relation;
+    String getName(){
         return name;
     }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-    public int getAge() {
+    int getAge(){
         return age;
     }
-
-
-    abstract  void familyMemberDetail();    //overloading
-}
-
-class Father extends Family{                 
-   private String profession;
-
-    public void setProfession(String profession) {
-        this.profession = profession;
-    }
-    public String getProfession(){
-        return profession;
-    }
-    Father(String n,int a,String p){
-        setName(n);
-        setAge(a);
-        setProfession(p);
-    }
-    void familyMemberDetail(){
-        System.out.println("||Father||");
-        System.out.println("Name:"+getName());
-        System.out.println("Age:"+getAge());
-        System.out.println("Profession:"+getProfession());
-        System.out.println();
+    public String toString(){
+        return "Relation: "+relation+", Name: "+name+", Age: "+age;
     }
 }
 
-class Mother extends Family{
-private String occupation;
-public void setOccupation(String occupation){
-    this.occupation=occupation;
+class Father extends FamilyMember{
+    Father(String n,int a,String r){
+        this.name=n;
+        this.age=a;
+        this.relation=r;
     }
-    public String getOccupation(){
-    return occupation;
+    String getName(){
+        return this.name;
     }
-    Mother(String n,int a,String o){
-        setName(n);
-        setAge(a);
-        setOccupation(o);
+    int getAge(){
+        return this.age;
     }
-    void familyMemberDetail(){
-        System.out.println("||Mother||");
-        System.out.println("Name:"+getName());
-        System.out.println("Age:"+getAge());
-        System.out.println("Profession:"+getOccupation());
-        System.out.println();
+}
+class Mother extends FamilyMember{
+    Mother(String n,int a,String r){
+        this.name=n;
+        this.age=a;
+        this.relation=r;
+    }
+    String getName(){
+        return this.name;
+    }
+    int getAge(){
+        return this.age;
+    }
+}
+class Sister extends FamilyMember{
+    Sister(String n,int a,String r){
+        this.name=n;
+        this.age=a;
+        this.relation=r;
+    }
+    String getName(){
+        return this.name;
+    }
+    int getAge(){
+        return this.age;
     }
 }
 
-class Sister extends Family{
-    private String qualification;
-    public void setQualification(String qualification){
-        this.qualification=qualification;
-    }
-    public String getQualification(){
-        return this.qualification;
-    }
-
-Sister(String n,int a,String q){
-        setName(n);
-        setAge(a);
-        setQualification(q);
-    }
-    void familyMemberDetail(){
-        System.out.println("||Sister||");
-        System.out.println("Name:"+getName());
-        System.out.println("Age:"+getAge());
-        System.out.println("Qualification:"+getQualification());
-        System.out.println();
-    }
-}
-
-class GrandMother extends Family{
-    private String ChronicIllness;
-
-    public void setChronicIllness(String ChronicIllness){
-        this.ChronicIllness=ChronicIllness;
-    }
-    public String getMedicinesTakenFor(){
-        return this.ChronicIllness;
-    }
-    GrandMother(String n,int a,String i){
-        setName(n);
-        setAge(a);
-        setChronicIllness(i);
-    }
-    void familyMemberDetail(){
-        System.out.println("||GrandMother||");
-        System.out.println("Name:"+getName());
-        System.out.println("Age:"+getAge());
-        System.out.println("Taking medicines for:"+getMedicinesTakenFor());
-        System.out.println();
-    }
-}
 public class DisplayFamily {
 
     public static void main(String[] args) {
-	Father fatherobject=new Father("Swapnil Rao",60,"BSNl employee");
-	Mother motherobject=new Mother("Swati Rao",51,"Family Manager");
-	Sister sisterobject=new Sister("Ananya Rao",26,"M.Tech");
-	GrandMother grandmotherobject=new GrandMother("Sumitra Rao",82,"Diabetes");
-	fatherobject.familyMemberDetail();
-	motherobject.familyMemberDetail();
-	sisterobject.familyMemberDetail();
-	grandmotherobject.familyMemberDetail();
+
+Father fatherobj=new Father("KK",62,"Father");
+Mother motherobj=new Mother("rajeshwari",50,"Mother");
+Sister sisterobj=new Sister("Ranjana",26,"Sister");
+        List<FamilyMember> arr=new ArrayList<FamilyMember>();
+       arr.add(fatherobj);
+       arr.add(motherobj);
+       arr.add(sisterobj);
+        for (FamilyMember i:arr
+             ) {
+            System.out.println(i);
+
+        }
+
+
     }
-}
+    }
+
